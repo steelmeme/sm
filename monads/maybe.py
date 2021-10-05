5f962db340c989904ee4c731ae3bda87332be4e9
steelmeme <91914292+steelmeme@users.noreply.github.com>
2021-10-05 11:44:51 +0100

    Maybe: add some helpers
    Adds:
    - is_just
    - is_nothing
    - from_just
    - from_maybe

--- monads/maybe.py
+++ monads/maybe.py
@@ -2,6 +2,26 @@ class Maybe:
     def __init__(self):
         pass
 
+    def is_just(self):
+        return self.__class__ == Just
+
+    def is_nothing(self):
+        return self.__class__ == Nothing
+
+    @staticmethod
+    def from_just(_maybe):
+        if _maybe.is_just():
+            return _maybe.value
+        else:
+            raise Exception('Maybe#from_just called with Maybe#Nothing')
+
+    @staticmethod
+    def from_maybe(_maybe, default):
+        if _maybe.is_nothing():
+            return default
+        else:
+            return _maybe.value
+
 class Nothing(Maybe):
     def __init__(self):
         pass
